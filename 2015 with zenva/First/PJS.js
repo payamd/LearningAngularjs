8cd0b4842588e6636806d03ae1d49cb3790086f4
payamd <payam.d7@gmail.com>
2014-12-01 11:05:16 -0800

    ppl need food so we need ng repeat ! :P

--- 2015 with zenva/First/PJS.js
+++ 2015 with zenva/First/PJS.js
@@ -16,4 +16,27 @@ app.controller("pctrl",['$scope',function($scope){
         $scope.sha.age --;
     };
 
+
+
+    $scope.items = [
+        {
+            name: 'pizza',
+            ingredients: ['cheese', 'tomato', 'oregano', 'salt']
+        },
+        {
+            name: 'tortilla',
+            ingredients: ['butter', 'salt', 'pepper', 'garlic']
+        },
+        {
+            name: 'cake',
+            ingredients: ['cream', 'sugar']
+        },
+        {
+            name: 'empanada',
+            ingredients: ['flour', 'meat', 'onion']
+        }
+    ];
+
+
+
 }]);
