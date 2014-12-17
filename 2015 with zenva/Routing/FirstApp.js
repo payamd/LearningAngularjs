08d7362c0b521075751a86a4d27089890770a045
payamd <payam.d7@gmail.com>
2014-12-17 12:40:48 -0800

    ID generating added

--- 2015 with zenva/Routing/FirstApp.js
+++ 2015 with zenva/Routing/FirstApp.js
@@ -10,8 +10,12 @@ app.config(function($routeProvider){
             templateUrl :'views/Items.html',
             controller:'ExpensesViewController'
         })
-        .when('/edit',{
+        .when('/edit/:id',{
             templateUrl :'views/Edit.html',
+            controller:'ExpensesEditViewController'
+        })
+        .when('/new',{
+            templateUrl :'views/new.html',
             controller:'ExpensesEditViewController'
         })
         .otherwise({redirectTO:'/'});
@@ -25,21 +29,65 @@ app.controller("pctrl",['$scope',function($scope){
 }]);/**
  * Created by Moon on 12/4/2014.
  */
+app.factory('Expenses',function(){
+   var service=[];
 
-
-
-app.controller('ExpensesViewController', ['$scope', function($scope) {
-
-    $scope.expenses = [
-        {description: 'food', amount: 10, date: '2014-10-01'},
-        {description: 'tickets', amount: 11, date: '2014-10-02'},
-        {description: 'food', amount: 12, date: '2014-10-03'},
-        {description: 'phone credit', amount: 13, date: '2014-10-04'},
-        {description: 'bills', amount: 14, date: '2014-10-05'},
-        {description: 'food', amount: 15, date: '2014-10-06'},
+    service.items=[
+        {id:1,description: 'food', amount: 80, date: '2014-10-01'},
+        {id:2,description: 'tickets', amount: 11, date: '2014-10-02'},
+        {id:3,description: 'food', amount: 12, date: '2014-10-03'},
+        {id:4,description: 'phone credit', amount: 13, date: '2014-10-04'},
+        {id:5,description: 'bills', amount: 14, date: '2014-10-05'},
+        {id:6,description: 'food', amount: 15, date: '2014-10-06'},
     ]
+
+
+    service.getNewId = function() {
+        if(service.newId) {
+            service.newId++;
+            return service.newId;
+        }
+        else {
+            var MaxId = _.max(service.items,function(item){return item.id;});
+            service.newId = MaxId.id + 1;
+            alert(service.newId);
+            return service.newId;
+        }
+    }
+
+
+
+    service.save=function(entry){
+        entry.id=service.getNewId();
+        service.items.push(entry)  ;
+    }
+    return service;
+});
+
+
+
+app.controller('ExpensesViewController', ['$scope','Expenses','$location',function($scope,Expenses,$location) {
+    $scope.expenses=Expenses.items;
+    $scope.addnew = function(){
+        $location.path('/new');
+
+    }
 }]);
 
-app.controller('ExpensesEditViewController',['$scope',function($scope){
-    $scope.welcome='you can edit your items from here';
+app.controller('ExpensesEditViewController',['$scope','$routeParams','Expenses','$location',function($scope , $routeParams,Expenses,$location){
+    $scope.welcome='you can edit your items from here ' + $routeParams.id + '\n the first item is ' + Expenses.items[0].description;
+
+
+if(!$routeParams.id){
+
+    $scope.Expenses = {id:'0' ,description: 'lazania', amount: 6, date: new Date().getFullYear()};
+}
+
+
+
+    $scope.save=function(){
+        Expenses.save($scope.Expenses);
+        $location.path('/');
+    }
+
 }])
